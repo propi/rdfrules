ee2294088c4c59a7c6e1de409135bcef9ba268d3
Václav Zeman <prozeman@gmail.com>
2018-03-27 10:59:55 +0200

    Cli removed

--- cli/src/main/scala/eu/easyminer/rdf/cli/State.scala
+++
@@ -1,8 +0,0 @@
-package eu.easyminer.rdf.cli
-
-/**
-  * Created by Vaclav Zeman on 7. 10. 2017.
-  */
-trait State {
-  val isTerminated: Boolean
-}
--- cli/src/main/scala/eu/easyminer/rdf/cli/impl/ComplexStateCommand.scala
+++
@@ -1,8 +0,0 @@
-package eu.easyminer.rdf.cli.impl
-
-import eu.easyminer.rdf.cli.Command
-
-/**
-  * Created by Vaclav Zeman on 7. 10. 2017.
-  */
-trait ComplexStateCommand extends Command[ComplexState]
